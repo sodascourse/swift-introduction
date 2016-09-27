5d748f88194fe4b06ce47ccd27c4c536173f6b47
sodastsai <sodas2002@gmail.com>
2016-09-27 15:21:27 +0800

    Add Variables (Lite)

---
+++ Swift-Introduction-Lite.playground/Pages/Introduction.xcplaygroundpage/Contents.swift
@@ -0,0 +1,22 @@
+/*:
+
+ # Introduction to the Swift language (Lite)
+ 
+ Swift is the new languague designed by Apple Inc. for the macOS, iOS, tvOS, and watchOS.
+ 
+ This playground is a material of the Mobile App Programming class at NCCU. For further information, 
+ see [course.sodas.tw](https://course.sodas.tw/)
+ 
+ ## Related Keynote Slides
+ 
+ - [Using Xcode Playground](https://www.icloud.com/keynote)
+ - [Introduction to Swift](https://www.icloud.com/keynote/0I68ruGtrQCL8O0W8AbWK-2_A)
+
+ ## References
+
+ * [The Swift Programming Language](https://developer.apple.com/library/ios/documentation/Swift/Conceptual/Swift_Programming_Language/index.html#//apple_ref/doc/uid/TP40014097-CH3-ID0)
+ * [Apple's "Learn the Essentials of Swift" Playground](https://developer.apple.com/library/prerelease/ios/referencelibrary/GettingStarted/DevelopiOSAppsSwift/Lesson1.html#//apple_ref/doc/uid/TP40015214-CH3-SW1)
+
+ ---
+ [next ->](@next)
+ */
