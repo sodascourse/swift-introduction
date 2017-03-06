3364826390aa35f3d396f78b0fb488c0e481a60a
sodastsai <sodas2002@gmail.com>
2017-03-06 10:33:47 +0800

    Add content

---
+++ Swift-Introduction.playground/Pages/Extensions.xcplaygroundpage/Contents.swift
@@ -0,0 +1,110 @@
+/*:
+
+ # Extensions
+
+ Extensions add new functionality to an existing `class`, `struct`, and `enum`.
+ It can also extend types for which you do not have access to the original source code.
+ 
+ Extensions in Swift can:
+
+ - Add computed instance properties and computed type properties
+ - Define instance methods and type methods
+ - Provide new initializers
+ - Define subscripts
+ - Define and use new nested types
+ - Make an existing type conform to a protocol
+
+ */
+
+//: --------------------------------------------------------------------------------------------------------------------
+/*:
+ 
+ ## Extend types
+
+ */
+extension Array {
+    var middle: Element? {
+        guard !self.isEmpty else {
+            return nil
+        }
+        return self[self.count/2]
+    }
+}
+
+[0, 1, 2].middle
+[1, 2, 3, 4].middle
+
+extension String {
+    mutating func prepend(_ string: String) {
+        self = string + self
+    }
+}
+var string = "Hello"
+string.prepend("+++ ")
+
+//: --------------------------------------------------------------------------------------------------------------------
+/*:
+ 
+ ## Group and Organize implementation code
+
+ */
+
+// Main type body
+struct Fraction {
+    var numerator: Int
+    var denominator: Int
+
+    init?(numerator: Int, denominator: Int) {
+        guard denominator != 0 else {
+            return nil
+        }
+        self.numerator = numerator
+        self.denominator = denominator
+    }
+}
+
+// Extra type features
+extension Fraction {
+    var doubleValue: Double {
+        return Double(self.numerator) / Double(self.denominator)
+    }
+}
+
+// Bridge for other types
+extension Int {
+    init(_ fraction: Fraction) {
+        self.init(fraction.doubleValue)
+    }
+}
+
+extension Double {
+    init(_ fraction: Fraction) {
+        self = fraction.doubleValue
+    }
+}
+
+// Adopt protocols
+extension Fraction: ExpressibleByIntegerLiteral {
+    init(integerLiteral value: Int) {
+        self.init(numerator: 1, denominator: value)!
+    }
+}
+
+extension Fraction: CustomStringConvertible {
+    var description: String {
+        return "Fraction(\(self.numerator)/\(self.denominator))"
+    }
+}
+
+// Using
+let oneThird = Fraction(numerator: 1, denominator: 3)!
+let fourThird = Fraction(numerator: 4, denominator: 3)!
+print(oneThird)  // CustomStringConvertible
+let two: Fraction = 2  // ExpressibleByIntegerLiteral
+Int(fourThird)
+Double(oneThird)
+
+//: ---
+//:
+//: [<- Previous](@previous) | [Next ->](@next)
+//:
